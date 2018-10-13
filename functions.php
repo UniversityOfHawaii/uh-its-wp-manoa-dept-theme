769d2833b59fc1e27f773a38f557e3c98b97cc9e
Michelle Carino <macarino@hawaii.edu>
2018-10-12 17:29:17 -1000

    remove support for post formats - not necessary

--- functions.php
+++ functions.php
@@ -65,9 +65,6 @@ if ( ! function_exists( 'manoa2018_setup' ) ) :
         // This theme styles the visual editor with editor-style.css to match the theme style.
         add_editor_style();
 
-        // Post Format support. You can also use the legacy "gallery" or "asides" (note the plural) categories.
-        add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
-
         // This theme uses post thumbnails
         add_theme_support( 'post-thumbnails' );
 
@@ -122,7 +119,7 @@ endif;
  *
  */
 function manoa2018_excerpt_length( $length ) {
-    return 40;
+    return 18;
 }
 add_filter( 'excerpt_length', 'manoa2018_excerpt_length' );
 
@@ -133,7 +130,7 @@ if ( ! function_exists( 'manoa2018_continue_reading_link' ) ) :
      *
      */
     function manoa2018_continue_reading_link() {
-        return ' <a href="' . get_permalink() . '">' . __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'manoa2018' ) . '</a>';
+        return ' ';
     }
 endif;
 
@@ -429,13 +426,13 @@ function course_theme_activate_create_default_content($old_name, $old_theme = fa
         ),
         array(
             'name' => "About",
-            'content' => "<p>Insert content about your department here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>",
+            'content' => "<p>Insert content about your department here. <a href='http://hawaii.edu/access/' title='accessibility at UH'>Accessibility notes</a></p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>",
         ),
         array(
             'name' => "Contact",
             'content' => "<p>You can insert your contact information here.</p>
+                <p>Visit us at:</p>
                 <address>
-                Visit us at:<br />
                 Office Name<br />
                 Address Line 1<br />
                 Address Line 2
