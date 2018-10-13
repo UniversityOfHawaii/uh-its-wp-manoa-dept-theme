76fe822f997fae2a7995a9d6985282a22b7fd384
Michelle Carino <macarino@hawaii.edu>
2018-10-12 17:28:57 -1000

    add entry title, content, sidebar to homepage

--- front-page.php
+++ front-page.php
@@ -18,9 +18,20 @@ get_header(); ?>
 		<div id="container">
 			<div id="content" role="main">
 
-				<h1></h1>
+				<?php
+				if ( have_posts() ) {
+					while ( have_posts() ) :
+						the_post();
+					?>
+
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+
+				<?php endwhile;
+				}; // end of the loop. ?>
 
 			</div><!-- #content -->
 		</div><!-- #container -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
