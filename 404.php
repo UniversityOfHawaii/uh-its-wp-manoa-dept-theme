9732fbf2f1322b55a78a1617c6434e861cc1fc48
Michelle Carino <macarino@hawaii.edu>
2018-11-07 09:12:30 -1000

    edit 404 error page

--- 404.php
+++ 404.php
@@ -4,24 +4,30 @@
  *
  */
 
-get_header(); ?>
+if ( get_theme_mod( 'header-option' ) == 'header2') :
+	get_header('other'); 
+else:
+	get_header();
+endif;
+?>
 
-	<div id="container">
-		<div id="content" role="main">
+	<main>
+		<div id="main_content">
+			<div id="container">
+				<div id="content" role="main">
 
-			<div id="post-0" class="post error404 not-found">
-				<h1 class="entry-title"><?php _e( 'Not Found', 'manoa2018' ); ?></h1>
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'manoa2018' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</div><!-- #post-0 -->
+					<div id="post-0" class="post error404 not-found">
+						<h1 class="entry-title"><?php _e( 'Sorry, this page could not be found.', 'manoa2018' ); ?></h1>
+						<div class="entry-content">
+							<p><?php _e( 'The page you are looking for does not exist, no longer exists or has been moved.', 'manoa2018' ); ?></p>
+						</div>
+					</div><!-- #post-0 -->
 
-		</div><!-- #content -->
-	</div><!-- #container -->
-	<script type="text/javascript">
-		// focus on search field after it has loaded
-		document.getElementById('s') && document.getElementById('s').focus();
-	</script>
+				</div><!-- #content -->
+			</div><!-- #container -->
+			<script type="text/javascript">
+				// focus on search field after it has loaded
+				document.getElementById('s') && document.getElementById('s').focus();
+			</script>
 
 <?php get_footer(); ?>
